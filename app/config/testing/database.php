6b600f58fa480e40d98f92f11209fe9b02cffaf5
Thomas Haddad <thomas.af.haddad@gmail.com>
2015-02-05 11:29:26 +0100

    travis 2.05

--- app/config/testing/database.php
+++ app/config/testing/database.php
@@ -1,12 +1,20 @@
 <?php
 
 return array(
-    'default'=>'mysql',
-    'connections'=>array(
-        'host'=>getenv('DB_HOST'),
-        'database'  => getenv('DB_NAME'),
-        'username'  => getenv('DB_USERNAME'),
-        'password'  => getenv('DB_PASSWORD'),
-    )
 
+    'default' => 'mysql',
+
+    'connections' => array(
+
+        'mysql' => array(
+            'driver'    => 'mysql',
+            'host'      => getenv('DB_HOST'),
+            'database'  => getenv('DB_NAME'),
+            'username'  => getenv('DB_USERNAME'),
+            'password'  => getenv('DB_PASSWORD'),
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+        ),
+    )
 );
